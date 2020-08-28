620c2010259bd0d2d3f9f79f3de44e1dccb1fe18
chicacode <gerahernan.al@gmail.com>
2020-08-28 13:35:42 +0200

    changing the logic

--- js/main.js
+++ js/main.js
@@ -8,45 +8,55 @@ console.log("Conected");
     The computer wins if it chooses scissors an the user chooses paper
 
 */
-
 let user_choice;
 let winner = "";
 let random;
 // 3 options
-let options = ["paper", "rock", "scissors"];
+
 // random Number fn
 function getRandomNumber() {
+    let options = ["paper", "rock", "scissors"];
     random = Math.floor(Math.random() * 3)
     let computer_choice = random; // cambiar nombre a vriable local a random_N
-    playGame(computer_choice);
-    return computer_choice; // We return the value of the local variable, not the local variable itself
+    options[computer_choice]
+
+    playGame(options[computer_choice]);
+    return options[computer_choice]; // We return the value of the local variable, not the local variable itself
+    // We return the value of the local variable, not the local variable itself
 }
+
 function playGame(computer_choice) {
-
     options = [computer_choice];
-    console.log("La opcion es " + options);
-    user_choice = parseInt(prompt("Choose an choose an option 0 PAPER , 1 ROCK , 2 SCISSORS"));
-    console.log(user_choice);
-    if (computer_choice == user_choice) {
-        document.getElementById("result").innerHTML = `There is a tie ${options = [computer_choice]}`;
+    console.log("La eleccion de la compu es " + options);
+    user_choice = prompt("Choose an choose an option paper, rock , scissors");
+    user_choice = user_choice.toLowerCase();
+    console.log("La eleccion del user es " + user_choice);
+
+    if (user_choice != "paper" && user_choice != "rock" && user_choice != "scissors") {
+        document.getElementById("result").innerHTML = `SORRY! YOU CHOOSE AN INVALID OPTION`;
     } else {
-        if (computer_choice == 0 && user_choice == 1) {
-
-            winner = "COMPUTER"
-            document.getElementById("result").innerHTML = `The winner is ${winner}  with ${options = [computer_choice]}`;
-        } else if (computer_choice == 1 && user_choice == 2) {
-
-            winner = "COMPUTER"
-            document.getElementById("result").innerHTML = `The Winner is ${winner}  with ${options = [computer_choice]}`;
-        } else if (computer_choice == 2 && user_choice == 0) {
-
-            winner = "COMPUTER"
-            document.getElementById("result").innerHTML = `The Winner is ${winner} with ${options = [computer_choice]}`;
-
+        if (computer_choice == user_choice) {
+            document.getElementById("result").innerHTML = `There is a tie. Both choose ${options = [computer_choice]}`;
         } else {
-            winner = "USER"
-            document.getElementById("result").innerHTML = `The Winner is ${winner}  with ${options = [computer_choice]}`;
+            if (computer_choice == "paper" && user_choice == "rock") {
+
+                winner = "COMPUTER"
+                document.getElementById("result").innerHTML = `The winner is ${winner}  with ${options = [computer_choice]}`;
+            } else if (computer_choice == "rock" && user_choice == "scissors") {
+
+                winner = "COMPUTER"
+                document.getElementById("result").innerHTML = `The Winner is ${winner}  with ${options = [computer_choice]}`;
+            } else if (computer_choice == "scissors" && user_choice == "paper") {
+
+                winner = "COMPUTER"
+                document.getElementById("result").innerHTML = `The Winner is ${winner} with ${options = [computer_choice]}`;
+
+            } else {
+                winner = "USER"
+                document.getElementById("result").innerHTML = `The Winner is ${winner}  with ${options = [computer_choice]}`;
+            }
         }
     }
+
 }
 
